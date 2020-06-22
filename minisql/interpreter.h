9e376360c2ff582dd40ae2583f16af17423469d8
JewdrunAleph <531678334@qq.com>
2020-06-23 03:00:48 +0800

    修改了因为文件名大小写变更导致的 Git bug

--- minisql/interpreter.h
+++ minisql/interpreter.h
@@ -1,6 +1,5 @@
-#ifndef _INTERPRETER_H
-#define _INTERPRETER_H
-
+#if !defined(_INTERPRETER_H_)
+#define _INTERPRETER_H_
 
 /* 致 interpreter 模块：
  * api 模块中对数据表、数据库、索引名的要求如下(基本同 mySQL 的要求)：
@@ -9,9 +8,12 @@
  * 希望在语法检查时注意。
  */
 
-// 目前这里只是列出了 api 模块需要的数据的格式。
-
+#include<iostream>
+//#include<stdlib.h>
+//#include<math.h>
+#include <algorithm>
 #include <string>
+
 using namespace std;
 
 // api 模块所需要的命令的格式。
@@ -24,5 +26,52 @@ struct command {
 	string arg2; // 命令的第二个参数。
 	string arg3; // 命令的第三个参数。
 };
+
+//获取用户输入，并对输入作有效性检查，若正确，返回语句的内部表示形式
+command Interpreter(string statement);
+//读取用户输入
+string read_input();
+//验证create语句是否有效
+command create_clause(string SQL, int start,command cmd);
+//验证create database语句是否有效
+command create_database(string SQL, int start,command cmd);
+//验证create table语句是否有效
+command create_table(string SQL, int start,command cmd);
+////获得属性
+//command get_attribute(string temp, string sql,command cmd);
+//验证create index语句是否有效
+command create_index(string SQL, int start,command cmd);
+//验证create index on语句是否有效
+command create_index_on(string SQL, int start, string sql,command cmd);
+//验证drop语句是否有效
+command drop_clause(string SQL, int start,command cmd);
+//验证drop database语句是否有效
+command drop_database(string SQL, int start,command cmd);
+//验证drop table语句是否有效
+command drop_table(string SQl, int start,command cmd);
+//验证drop index语句是否有效
+command drop_index(string SQL, int start,command cmd);
+//验证select 语句是否有效
+command select_clause(string SQL, int start,command cmd);
+////获得属性组或文件组的每一项
+//string get_part(string temp, string sql, string kind);
+//验证insert 语句是否有效
+command insert_clause(string SQL, int start,command cmd);
+//验证insert into values语句是否有效
+command insert_into_values(string SQL, int start, string sql,command cmd);
+//验证delete语句是否有效
+command delete_clause(string SQL, int start,command cmd);
+//验证 delete from where 语句是否有效
+command delete_from_where(string SQL, int start, string sql,command cmd);
+////将表达式转化为内部形式
+//string get_expression(string temp, string sql);
+////获取表达式组的每个表达式
+//string get_each(string T, string sql, string condition);
+//验证use语句是否有效
+command use_clause(string SQL, int start,command cmd);
+//验证execfile语句是否有效
+command execfile_clause(string SQL, int start,command cmd);
+//验证quit语句是否有效
+command quit_clause(string SQL, int start,command cmd);
 
 #endif
